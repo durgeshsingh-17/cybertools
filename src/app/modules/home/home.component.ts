fac2362d30b3c3657921fd161e13dc1836c291d4
Ashutosh Singh <64886185+AshutoshSingh2020@users.noreply.github.com>
2023-11-12 00:48:52 +0530

    home done

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  homeData:any[]=[
+    {image:'https://ezytool.com/assets/img/merge.svg',title:'Merge Pdf',body:'Combine Pdf in easiest way on in the order you want merger available.'},
+    {image:'https://ezytool.com/assets/img/pdftoimg.svg',title:'Merge Pdf',body:'Combine Pdf in easiest way on in the order you want merger available.'},
+    {image:'https://ezytool.com/assets/img/ocr.svg',title:'Split Pdf',body:'Combine Pdf in easiest way on in the order you want merger available.'},
+    {image:'https://ezytool.com/assets/img/merge.svg',title:'Add Pdf',body:'Combine Pdf in easiest way on in the order you want merger available.'},
+    {image:'https://ezytool.com/assets/img/emi-calculate.png',title:'Merge Pdf',body:'Combine Pdf in easiest way on in the order you want merger available.'},
+    {image:'https://ezytool.com/assets/img/merge.svg',title:'Block Pdf',body:'Combine Pdf in easiest way on in the order you want merger available.'},
+    {image:'https://ezytool.com/assets/img/ocr.svg',title:'Merge Pdf',body:'Combine Pdf in easiest way on in the order you want merger available.'},
+    {image:'https://ezytool.com/assets/img/pdftoimg.svg',title:'Solid Pdf',body:'Combine Pdf in easiest way on in the order you want merger available.'},
+
+  ];
+  
+  searchTerm: string = '';
+  
+    get filteredData(): any[] {
+      return this.homeData.filter(item => item.title.toLowerCase().includes(this.searchTerm.toLowerCase()));
+    }
+
 }

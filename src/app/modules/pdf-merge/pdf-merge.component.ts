94ca68c45da65c0b9bf1110998daecf7c0e22def
Ashutosh Singh <64886185+AshutoshSingh2020@users.noreply.github.com>
2024-01-05 23:48:22 +0530

    changed

--- src/app/modules/pdf-merge/pdf-merge.component.ts
+++ src/app/modules/pdf-merge/pdf-merge.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { PDFDocument } from 'pdf-lib';
+import { PDFDocument, degrees } from 'pdf-lib';
+import { PDFDocumentProxy } from 'ng2-pdf-viewer';
 @Component({
   selector: 'app-pdf-merge',
   templateUrl: './pdf-merge.component.html',
@@ -11,9 +12,12 @@ export class PdfMergeComponent implements OnInit{
   pdfMerge !:FormGroup;
   selectedFile!: File  ;
   selectedFile1!: File ;
+  pdfSrc:any;
+  pdfPages:any;
    link!:string;
    downloadLink:boolean = false;
    mergedPdfData:any;
+   rotationArray: { rotate: number; degree: number }[] = [];
    constructor(private fb:FormBuilder,private http: HttpClient){
  
    }
@@ -44,6 +48,7 @@ export class PdfMergeComponent implements OnInit{
   }
   async mergePdfs(pdfData1: Uint8Array, pdfData2: Uint8Array): Promise<Uint8Array> {
     try {
+
       const pdfDoc1 = await PDFDocument.load(pdfData1);
       const pdfDoc2 = await PDFDocument.load(pdfData2);
       const mergedPdf = await PDFDocument.create();
@@ -51,13 +56,37 @@ export class PdfMergeComponent implements OnInit{
       const pages2 = await mergedPdf.copyPages(pdfDoc2, pdfDoc2.getPageIndices());
   
       for (const page of pages1) {
+        console.log("Ashu")
         mergedPdf.addPage(page);
       }
-  
+      
       for (const page of pages2) {
+        console.log("kumar")
+      
+
         mergedPdf.addPage(page);
       }
-  
+      if (this.rotationArray.length > 0) {
+        let pageIndicesToModify = this.rotationArray.map(item => item.rotate);
+       
+        let pagesToModify = await mergedPdf.copyPages(mergedPdf, pageIndicesToModify);
+          console.log(pagesToModify);
+        for (let i = 0; i < pagesToModify.length; i++) {
+          const cpage = pagesToModify[i];
+          const rotationInfo = this.rotationArray.find(item => item.rotate === pageIndicesToModify[i]);
+      
+          if (rotationInfo) {
+            cpage.setRotation(degrees(rotationInfo.degree));
+            const pageIndex = pageIndicesToModify[i];
+            mergedPdf.removePage(pageIndex);
+            mergedPdf.insertPage(pageIndex, pagesToModify[i]);
+          }
+        }
+      }
+
+      this.pdfPages = mergedPdf.getPageIndices();
+
+      console.log( mergedPdf.getPageIndices());
       const mergedPdfBytes = await mergedPdf.save();
       return mergedPdfBytes;
     } catch (error) {
@@ -71,6 +100,9 @@ export class PdfMergeComponent implements OnInit{
       const pdfData2 = await this.selectedFile1.arrayBuffer();
       try {
          this.mergedPdfData = await this.mergePdfs(new Uint8Array(pdfData1), new Uint8Array(pdfData2));
+         const blob = new Blob([this.mergedPdfData], { type: 'application/pdf' });
+         const dataUrl = URL.createObjectURL(blob);
+         this.pdfSrc = dataUrl;
          this.downloadLink = true;
         } catch (error) {
           console.error('Error merging PDFs:', error);
@@ -80,11 +112,33 @@ export class PdfMergeComponent implements OnInit{
     }
   }
   downLoad(){
+    
+    console.log(this.rotationArray);
     const blob = new Blob([this.mergedPdfData], { type: 'application/pdf' });
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
     link.download = 'merged.pdf';
     this.link = link.href;
     link.click();
+  }
+  afterLoadComplete(pdf: PDFDocumentProxy) {
+    // this.totalPages = pdf.numPages;
+    // console.log(this.totalPages);
+  }
+
+  rotatePage(index:number){
+    let existingRotationIndex = this.rotationArray.findIndex(item => item.rotate === index);
+    
+    if (existingRotationIndex !== -1) {
+      // If rotation for this index already exists, modify the existing entry
+      this.rotationArray[existingRotationIndex].degree = (this.rotationArray[existingRotationIndex].degree + 90) % 360;
+    } else {
+      // If rotation for this index does not exist, add a new entry
+      this.addRotationInfo(index, 90);
+    }
+    this.onUpload();
+  }
+  addRotationInfo(rotate: number, degree: number): void {
+    this.rotationArray.push({ rotate,degree });
   }
 }
